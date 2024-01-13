eede428d2e6581420627a8b87b826ad678edd29f
jlxfd <julialexisfd@gmail.com>
2024-01-14 00:44:51 +0800

    Initialized the project

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="app-header"></header>
+      <div className="nav-bar nav-1">
+        <div className="link-tile"></div>
+        <div className="link-tile"></div>
+        <div className="link-tile"></div>
+        <div className="link-tile"></div>
+        <div className="link-tile"></div>
+      </div>
+      <div className="container-ad">
+        <div className="hero-ad"></div>
+      </div>
+      <div className="nav-bar nav-2">
+        <div className="link-tile link-tile-2 tile-1 "></div>
+        <div className="link-tile link-tile-2 "></div>
+        <div className="link-tile link-tile-2 "></div>
+        <div className="link-tile link-tile-2 "></div>
+        <div className="link-tile link-tile-2 "></div>
+        <div className="link-tile link-tile-2 "></div>
+        <div className="link-tile link-tile-2 "></div>
+      </div>
+      <div className="container-grid general">
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+      </div>      
+      <div className="container-ad container-ad-2">
+        <div className="hero-ad"></div>
+      </div>
+      <div className="nav-bar nav-2 sports">
+        <div className="link-tile link-tile-2 tile-1-3 "></div>
+        <div className="link-tile link-tile-2 "></div>
+        <div className="link-tile link-tile-2 "></div>
+        <div className="link-tile link-tile-2 "></div>
+        <div className="link-tile link-tile-2 "></div>
+        <div className="link-tile link-tile-2 "></div>
+        <div className="link-tile link-tile-2 "></div>
+      </div>
+      <div className="container-grid grid-sports">
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
+}
+
+export default App;
